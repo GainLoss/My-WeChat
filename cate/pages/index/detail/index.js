d480b7f64cf4fa2013d40d9c5d687cd0aa443538
zyxhaha <zyx930719@126.com>
2017-07-19 09:58:22 +0800

    微信小程序前台页面

---
+++ cate/pages/index/detail/index.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    mark:0
+  },
+  onLoad:function(options){
+    this.setData({
+      mark: options.mark
+    })
+  }
+})
---
+++ cate/pages/index/index.js
@@ -0,0 +1,62 @@
+//index.js
+//获取应用实例
+
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    hasLocation: false,
+    listImage:[
+      {mark:'one',src:'././image/1.jpeg'},
+      { mark: 'two',src: '././image/2.jpeg'},
+      { mark: 'three', src: '././image/3.png' },
+      { mark: 'four',src: '././image/4.png' },
+      { mark: 'five', src: '././image/5.jpeg' },
+      { mark: 'six', src: '././image/6.png' },
+    ]
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  //获取经纬度
+  getLocation: function (e) {
+    console.log(e)
+    var that = this
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      }
+    })
+  },
+})
